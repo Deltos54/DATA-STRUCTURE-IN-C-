d78bd3ef1dccd55fbb574bc901776a2d3811d18a
Deltos54 <2024A6R048@mietjammu.in>
2025-03-11 00:08:44 +0530

    Create 06 Two sum .c

---
+++ 06 Two sum .c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(){
+
+int n, i, j, target;
+scanf("%d", &n);
+int arr[n];
+
+for(i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+}
+scanf("%d", &target);
+for(i=0;i<n;i++){
+    for(j=i+1;j<n;j++){
+        if(arr[i]+arr[j]==target){
+            printf("%d %d", i, j);
+            break;
+        }
+    }}
+    return 0;
+}
